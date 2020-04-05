fa38c50fee809766e1d3c283e46be4b6d68461a6
Luiz Borges <luiiz.bgs@hotmail.com>
2020-04-05 17:01:11 -0300

    Adicionar arquivos de projeto.

---
+++ src/Intermaritima.ApplicationCore/Entity/Estabelecimento.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Intermaritima.ApplicationCore.Entity
+{
+    public class Estabelecimento
+    {
+        public Estabelecimento()
+        {
+
+        }
+        [Required]
+        [Range(3, 3)]
+        public virtual Char IdEstabelecimento { get; set; }//  cod-estabel
+        [Required]
+        [Range(8, 8)]
+        public virtual Char ContaContabil { get; set; }//    ct-codigo
+        [Required]
+        [Range(8, 8)]
+        public virtual Char CentroDeCusto { get; set; }//    centro-custo
+        [Required]
+        [Range(60, 60)]
+        public virtual Char HistoricoDoLancamento { get; set; }//    historico
+        [Required]
+        public virtual DateTime DataEmissao { get; set; }//    dt-emissao
+        [Required]
+        [Range(5, 5)]
+        public virtual Char Serie { get; set; }//    serie-docto
+        [Required]
+        [Range(16, 16)]
+        public virtual Char Especie { get; set; }//    cod-esp
+        [Required]
+        [Range(16, 16)]
+        public virtual Char Titulo { get; set; }//    nro-docto
+        [Required]
+        [Range(6, 6)]
+        public virtual Char Cfop { get; set; }//    nat-operacao
+        [Required]
+        public virtual int Emitente { get; set; }//    cod-emitente
+        [Required]
+        [Range(80, 80)]
+        public virtual Char Nome { get; set; }//    nome-fornec
+        [Required]
+        public virtual int Requisicao { get; set; }//    nr-requisicao
+        [Required]
+        public virtual int Seq { get; set; }//    sequencia
+        [Required]
+        [Range(16, 16)]
+        public virtual Char NumNF { get; set; }//    nr-nota
+        [Required]
+        [Range(16, 16)]
+        public virtual Char CodItem { get; set; }//    it-codigo
+        [Required]
+        [Range(3, 3)]
+        public virtual Decimal ValorDebito { get; set; }//    vl-debito
+        [Required]
+        public virtual Decimal ValorCredito { get; set; }//    vl-credito
+        [Required]
+        public virtual Decimal Quantidade { get; set; }//    qt-requisitada
+
+               
+    }
+}
---
+++ src/Intermaritima.UI.Web/Controllers/HomeController.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Intermaritima.UI.Web.Models;
+using Intermaritima.ApplicationCore.Entity;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Intermaritima.UI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IConfiguration Configuration { get; }
+
+        public IActionResult Index()
+        {
+            List<Estabelecimento> estabelecimentoList = new List<Estabelecimento>();
+
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                //SqlDataReader
+                connection.Open();
+
+                string sql = "Select * From Teacher";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Estabelecimento estabelecimento = new Estabelecimento();
+
+                        estabelecimento.IdEstabelecimento = Convert.ToChar(dataReader["cod-estabel"]);
+                        estabelecimento.ContaContabil = Convert.ToChar(dataReader["ct-codigo"]);
+                        estabelecimento.CentroDeCusto = Convert.ToChar(dataReader["centro-custo"]);
+                        estabelecimento.HistoricoDoLancamento = Convert.ToChar(dataReader["historico"]);
+                        estabelecimento.DataEmissao = Convert.ToDateTime(dataReader["dt-emissao"]);
+                        estabelecimento.Serie = Convert.ToChar(dataReader["serie-docto"]);
+                        estabelecimento.Especie = Convert.ToChar(dataReader["cod-esp"]);
+                        estabelecimento.Titulo = Convert.ToChar(dataReader["nro-docto"]);
+                        estabelecimento.Cfop = Convert.ToChar(dataReader["nat-operacao"]);
+                        estabelecimento.Emitente = Convert.ToInt32(dataReader["cod-emitente"]);
+                        estabelecimento.Nome = Convert.ToChar(dataReader["nome-fornec"]);
+                        estabelecimento.Requisicao = Convert.ToInt32(dataReader["nr-requisicao"]);
+                        estabelecimento.Seq = Convert.ToInt32(dataReader["sequencia"]);
+                        estabelecimento.NumNF = Convert.ToChar(dataReader["nr-nota"]);
+                        estabelecimento.CodItem = Convert.ToChar(dataReader["it-codigo"]);
+                        estabelecimento.ValorDebito = Convert.ToDecimal(dataReader["vl-debito"]);
+                        estabelecimento.ValorCredito = Convert.ToDecimal(dataReader["vl-credito"]);
+                        estabelecimento.Quantidade = Convert.ToDecimal(dataReader["qt-requisitada"]);
+                    }
+                }
+
+                connection.Close();
+            }
+            return View(estabelecimentoList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        [HttpPost]
+        [ActionName("Create")]
+        public IActionResult Create(Estabelecimento estabelecimento)
+        {
+
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Insert Into es-ana-contab (cod-estabel,ct-codigo,centro-custo,historico,dt-emissao,serie-docto,cod-esp,nro-docto,nat-operacao,cod-emitente,nome-fornec,nr-requisicao,sequencia, nr-nota,it-codigo,vl-debito,vl-credito,qt-requisitada) Values ('{estabelecimento.IdEstabelecimento}','{estabelecimento.ContaContabil}','{estabelecimento.CentroDeCusto}','{estabelecimento.HistoricoDoLancamento}','{estabelecimento.DataEmissao}','{estabelecimento.Serie}','{estabelecimento.Especie}','{estabelecimento.Titulo}','{estabelecimento.Cfop}','{estabelecimento.Emitente}','{estabelecimento.Nome}','{estabelecimento.Requisicao}','{estabelecimento.Seq}','{estabelecimento.NumNF}','{estabelecimento.CodItem}','{estabelecimento.ValorDebito}','{estabelecimento.ValorCredito}','{estabelecimento.Quantidade}')";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.CommandType = CommandType.Text;
+
+                    connection.Open();
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        ViewBag.Result = "Erro:" + ex.Message;
+                        return RedirectToAction("Index");
+
+                    }
+                    connection.Close();
+                }
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public IActionResult Update(Estabelecimento estabelecimento)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Update es-ana-contab SET ct-codigo='{estabelecimento.ContaContabil}', centro-custo='{estabelecimento.CentroDeCusto}',  historico='{estabelecimento.HistoricoDoLancamento}', dt-emissao='{estabelecimento.DataEmissao}',serie-docto='{estabelecimento.Serie}', cod-esp='{estabelecimento.Especie}', nro-docto='{estabelecimento.Titulo}', nat-operacao='{estabelecimento.Cfop}', cod-emitente='{estabelecimento.Emitente}', nome-fornec='{estabelecimento.Nome}', nr-requisicao='{estabelecimento.Requisicao}', sequencia='{estabelecimento.Seq}', nr-nota='{estabelecimento.NumNF}', it-codigo='{estabelecimento.CodItem}', vl-debito='{estabelecimento.ValorDebito}', vl-credito='{estabelecimento.ValorCredito}', qt-requisitada='{estabelecimento.Quantidade}' Where cod-estabel ='{estabelecimento.IdEstabelecimento}'";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        ViewBag.Result = "Erro:" + ex.Message;
+                    }
+                    connection.Close();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult Delete(int id)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Delete From es-ana-contab Where Id='{id}'";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        ViewBag.Result = "Erro:" + ex.Message;
+                    }
+                    connection.Close();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ActionName("Select")]
+        public IActionResult Select(int id)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Select * From es-ana-contab Where Id='{id}'";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        ViewBag.Result = "Erro:" + ex.Message;
+                    }
+                    connection.Close();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult ssa()
+        {
+            List<Estabelecimento> estabelecimentoList = new List<Estabelecimento>();
+
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "Select * From es-ana-contab";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Estabelecimento estabelecimento = new Estabelecimento();
+
+                        estabelecimento.IdEstabelecimento = Convert.ToChar(dataReader["cod-estabel"]);
+                        estabelecimento.ContaContabil = Convert.ToChar(dataReader["ct-codigo"]);
+                        estabelecimento.CentroDeCusto = Convert.ToChar(dataReader["centro-custo"]);
+                        estabelecimento.HistoricoDoLancamento = Convert.ToChar(dataReader["historico"]);
+                        estabelecimento.DataEmissao = Convert.ToDateTime(dataReader["dt-emissao"]);
+                        estabelecimento.Serie = Convert.ToChar(dataReader["serie-docto"]);
+                        estabelecimento.Especie = Convert.ToChar(dataReader["cod-esp"]);
+                        estabelecimento.Titulo = Convert.ToChar(dataReader["nro-docto"]);
+                        estabelecimento.Cfop = Convert.ToChar(dataReader["nat-operacao"]);
+                        estabelecimento.Emitente = Convert.ToInt32(dataReader["cod-emitente"]);
+                        estabelecimento.Nome = Convert.ToChar(dataReader["nome-fornec"]);
+                        estabelecimento.Requisicao = Convert.ToInt32(dataReader["nr-requisicao"]);
+                        estabelecimento.Seq = Convert.ToInt32(dataReader["sequencia"]);
+                        estabelecimento.NumNF = Convert.ToChar(dataReader["nr-nota"]);
+                        estabelecimento.CodItem = Convert.ToChar(dataReader["it-codigo"]);
+                        estabelecimento.ValorDebito = Convert.ToDecimal(dataReader["vl-debito"]);
+                        estabelecimento.ValorCredito = Convert.ToDecimal(dataReader["vl-credito"]);
+                        estabelecimento.Quantidade = Convert.ToDecimal(dataReader["qt-requisitada"]);
+
+                        estabelecimentoList.Add(estabelecimento);
+                    }
+                }
+
+                connection.Close();
+            }
+            return View(estabelecimentoList);
+        }
+    }
+}
